ad95d0b2572c59f7f359efcd0fafb90397d96cf4
ZinaienkovaHanna <zinaienkova.anna@gmail.com>
2024-01-11 12:56:33 +0200

    added footer

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 import Menu from '../Menu';
 import Search from '../Search';
 import Dropdown from '../Dropdown';
+import Footer from '../Footer';
 import namesCocktails from '../../mockedData/autocompleteCocktails.json';
 import namesIngredients from '../../mockedData/autocompleteIngredients.json';
 import namesCategories from '../../mockedData/categories.json';
@@ -11,16 +12,19 @@ import styles from './Sidebar.module.css';
 const Sidebar: FC = () => {
     return (
         <div className={styles.container}>
-            <Menu />
-            <Search
-                placeholder="Search by name"
-                autocompleteData={namesCocktails}
-            />
-            <Search
-                placeholder="Search by ingredients"
-                autocompleteData={namesIngredients}
-            />
-            <Dropdown categories={namesCategories} />
+            <div>
+                <Menu />
+                <Search
+                    placeholder="Search by name"
+                    autocompleteData={namesCocktails}
+                />
+                <Search
+                    placeholder="Search by ingredients"
+                    autocompleteData={namesIngredients}
+                />
+                <Dropdown categories={namesCategories} />
+            </div>
+            <Footer />
         </div>
     );
 };
